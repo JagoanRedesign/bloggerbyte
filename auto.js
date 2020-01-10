d58466b0f886f5fd1822a59c9c38f2e05934ae60
rooplal <45243087+rooplal@users.noreply.github.com>
2020-01-10 19:09:37 +0530

    Update auto.js

--- auto.js
+++ auto.js
@@ -21,10 +21,10 @@ if(a_to_vh==false)
 {var encryptedUrl=Base64.encode(a_to_vi);
 var x=Math.floor((Math.random()*4)+ 1);
 var xxx=null;
-if(x=="1"){xxx="college-credit-card.html"}
-if(x=="2"){xxx="apply-online-for-credit-card.html"}
-if(x=="3"){xxx="consolidate-credit-card.html"}
-if(x=="4"){xxx="business-credit-card.html"}a_to_ve[i].href="https://www.ebooksfree4u.com/p/page"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="1"){xxx="page.html"}
+if(x=="2"){xxx="page.html"}
+if(x=="3"){xxx="page.html"}
+if(x=="4"){xxx="page.html"}a_to_ve[i].href="https://www.ebooksfree4u.com/p/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
