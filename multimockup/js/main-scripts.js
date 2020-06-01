47c1e54bed9c8b6172f4b355afa6987cd357a623
rooplal <45243087+rooplal@users.noreply.github.com>
2020-06-01 16:27:13 +0530

    Create main-scripts.js

---
+++ multimockup/js/main-scripts.js
@@ -0,0 +1,13 @@
+<script src="https://cdn.statically.io/gh/rooplal/bloggerbyte/d7157d52/multimockup/js/jquery-ui.min.js"></script>
+<!--switchery-->
+<script src="https://cdn.statically.io/gh/rooplal/bloggerbyte/master/multimockup/js/switchery.min.js"></script>
+<!--spectrum-->
+<script src="https://cdn.statically.io/gh/rooplal/bloggerbyte/master/multimockup/js/spectrum.js"></script>
+<!--jquery transit-->
+<script type="text/javascript" src="https://cdn.statically.io/gh/rooplal/bloggerbyte/master/multimockup/js/jquery.transit.min.js"></script>
+<!--jquery transform-->
+<script type="text/javascript" src="https://cdn.statically.io/gh/rooplal/bloggerbyte/master/multimockup/js/jquery.transform2d.js"></script>
+<!--Custom JS-->
+<script type="text/javascript" src="https://cdn.statically.io/gh/rooplal/bloggerbyte/master/multimockup/js/customjs.js"></script>
+<!-- Include all compiled plugins (below), or include individual files as needed -->
+<script src="https://cdn.statically.io/gh/rooplal/bloggerbyte/master/multimockup/js/bootstrap.bundle.min.js"></script>
